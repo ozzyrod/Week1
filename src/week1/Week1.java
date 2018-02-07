d40cb86894fbc5809a5a7beac210ecb4e5098019
Ozzy Rodriguez <ozzy@ozzyrodriguez.com>
2018-02-06 19:31:58 -0800

    Initial Commit

---
+++ src/week1/Week1.java
@@ -0,0 +1,23 @@
+/*
+ *
+ * Project: Week1
+ * Author:  Osbardo Rodriguez
+ * Date:    2/6/18
+ */
+package week1;
+
+/**
+ *
+ */
+public class Week1 {
+
+	/**
+	 *
+	 * @param args command line arguments
+	 */
+	public static void main( String[] args ) {
+		System.out.println( "Hello World" );
+		System.out.print( "This is on one line" );
+		System.out.println( " and then I go to the next line" );
+	}
+}
